3469de0c1bdf108e7bed3596a3164a8e3831c338
yashbhangdia <55742719+yashbhangdia@users.noreply.github.com>
2021-07-06 11:05:35 +0530

    Create data.groovy

---
+++ vars/data.groovy
@@ -0,0 +1,11 @@
+#!usr/bin/env groovy
+
+def vm()
+{
+   return [ 1 : ["virtual_machine_1", "5c832d8a-57a6-405f-a1be-7eb0b12d517d", "192.168.50.50"], 2 : ["virtual_machine_2", "Jenkins_2", "192.168.51.51"], 3 : ["virtual_machine_3", "VirtualMachine3", "192.168.52.52"]]
+}
+
+def project()
+{
+   return ["https://github.com/yashbhangdia/spring-boot-maven-example-helloworld.git" : [1, 2], "https://github.com/yashbhangdia/SpringBoot-Application-2.git" : [3]] 
+}
