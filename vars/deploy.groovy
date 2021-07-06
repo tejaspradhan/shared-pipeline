826b1bb091f68b10bc36e7d8bc11c85fa8b6181f
yashbhangdia <55742719+yashbhangdia@users.noreply.github.com>
2021-07-06 11:09:14 +0530

    Create deploy.groovy

---
+++ vars/deploy.groovy
@@ -0,0 +1,23 @@
+#!usr/bin/env groovy
+
+def call(url)
+{
+  def remote = getVM("${url}")
+  
+  remote.each{
+        item -> L:{
+            echo "Deploying to : ${item.name}"
+
+            withCredentials([sshUserPrivateKey(credentialsId: item.id, keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: item.name)]) {
+          item.user = item.name
+          item.identityFile = identity
+          sshCommand remote: item, command: 'rm -R deployment', failOnError:'false'
+          sshCommand remote: item, command: 'mkdir deployment', failOnError:false
+          sshCommand remote : item, command: 'cd /home/'+"${item.name}"+'/deployment; PID=`ps -eaf | grep "java -jar" | grep -v grep | awk \'{print $2}\'`; if [[ "" !=  "$PID" ]]; then echo "found $PID and killing it!!" ; kill -9 $PID; fi'
+          sshPut remote: item, from: "./target", filterRegex: /.war$/, into: '/home/'+"${item.name}"+'/deployment/'
+          sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/deployment/target; nohup java -jar *.war >> app.log &'
+            }
+        }
+    }
+  
+}
