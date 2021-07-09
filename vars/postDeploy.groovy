3fffcb2b98d13ac236133122433739e019e169e6
Shriram Pareek <66828879+Shrirampareek888@users.noreply.github.com>
2021-07-09 11:44:37 +0530

    war to jar

--- vars/postDeploy.groovy
+++ vars/postDeploy.groovy
@@ -14,7 +14,7 @@ def call(url)
             item.identityFile = identity
 
             sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/deployment; rm -R temp', failOnError:'false'
-            sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/deployment; mkdir temp; cd temp; jar xvf ../target/*.war'
+            sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/deployment; mkdir temp; cd temp; jar xvf ../target/*.jar'
             sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/deployment/temp/META-INF/maven/org.springframework.boot/SpringBootMavenExample; ls'
             sshGet remote: item, from:'deployment/temp/META-INF/maven/org.springframework.boot/SpringBootMavenExample/pom.properties', into: 'info/'+"${item.name}"+"-"+"${item.host}"+'.txt', override: true
             sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/deployment; rm -R temp', failOnError:'false'
