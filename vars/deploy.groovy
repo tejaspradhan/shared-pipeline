923110631c8bd7ad8d25729961925e9c386977e3
yashbhangdia <55742719+yashbhangdia@users.noreply.github.com>
2021-07-15 16:06:08 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -14,7 +14,7 @@ def call(url)
           withCredentials([sshUserPrivateKey(credentialsId: item.id, keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: item.name)]) {
           item.user = item.name
           item.identityFile = identity
-          sshCommand remote : item, command: 'cd /home/'+"${item.name}"+'/'+"${appname}"+'/target; TEMP=`find *.jar | awk \'{print $1}\'`; PID=`ps -eaf | grep "java -jar $TEMP" | grep -v grep | awk \'{print $2}\'`; if [[ "" !=  "$PID" ]]; then echo "found $PID and killing it!!" ; kill -9 $PID; fi'
+          sshCommand remote : item, command: 'cd /home/'+"${item.name}"+'/'+"${appname}"+'/target; TEMP=`find *.jar | awk \'{print $1}\'`; PID=`ps -eaf | grep "$TEMP" | grep -v grep | awk \'{print $2}\'`; if [[ "" !=  "$PID" ]]; then echo "found $PID and killing it!!" ; kill -9 $PID; fi'
           sshCommand remote: item, command: 'rm -R '+"${appname}", failOnError:'false'
           sshCommand remote: item, command: 'mkdir '+"${appname}", failOnError:false
           sshPut remote: item, from: "./target", filterRegex: /.jar$/, into: '/home/'+"${item.name}"+'/'+"${appname}"+'/'
