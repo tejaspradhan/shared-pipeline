a05753f792836b39e2ca1bd4ded2e14bc4afd281
yashbhangdia <55742719+yashbhangdia@users.noreply.github.com>
2021-07-15 10:37:18 +0530

    Update postDeploy.groovy

--- vars/postDeploy.groovy
+++ vars/postDeploy.groovy
@@ -4,6 +4,9 @@ def call(url)
 {
   def remote  = getVM("${url}")
   
+  def applist = data.app()
+  def appname = applist["${url}"]
+  
   bat 'IF EXIST info RMDIR /S /Q info'
   bat 'mkdir info'
 
@@ -13,11 +16,11 @@ def call(url)
             item.user = item.name
             item.identityFile = identity
 
-            sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/deployment; rm -R temp', failOnError:'false'
-            sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/deployment; mkdir temp; cd temp; jar xvf ../target/*.jar'
-            sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/deployment/temp/META-INF/maven/org.springframework.boot/SpringBootMavenExample; ls'
-            sshGet remote: item, from:'deployment/temp/META-INF/maven/org.springframework.boot/SpringBootMavenExample/pom.properties', into: 'info/'+"${item.name}"+"-"+"${item.host}"+'.txt', override: true
-            sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/deployment; rm -R temp', failOnError:'false'
+            sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/'+"${appname}"+'; rm -R temp', failOnError:'false'
+            sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/'+"${appname}"+'; mkdir temp; cd temp; jar xvf ../target/*.jar'
+            sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/'+"${appname}"+'/temp/META-INF/maven/org.springframework.boot/SpringBootMavenExample; ls'
+            sshGet remote: item, from:''+"${appname}"+'/temp/META-INF/maven/org.springframework.boot/SpringBootMavenExample/pom.properties', into: 'info/'+"${item.name}"+"-"+"${item.host}"+'.txt', override: true
+            sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/'+"${appname}"+'; rm -R temp', failOnError:'false'
           }
       }
   }
