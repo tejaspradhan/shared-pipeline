fa7a8fd5653ff74dbae4053acf2d2c3787c90ea4
yashbhangdia <55742719+yashbhangdia@users.noreply.github.com>
2021-07-15 17:52:07 +0530

    Update postDeploy.groovy

--- vars/postDeploy.groovy
+++ vars/postDeploy.groovy
@@ -2,6 +2,9 @@
 
 def call(url)
 {
+  def pom = readMavenPom file: 'pom.xml'
+  def artifact = pom.artifactId
+  
   def remote  = getVM("${url}")
   
   def applist = data.app()
@@ -18,8 +21,8 @@ def call(url)
 
             sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/'+"${appname}"+'; rm -R temp', failOnError:'false'
             sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/'+"${appname}"+'; mkdir temp; cd temp; jar xvf ../target/*.jar'
-            sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/'+"${appname}"+'/temp/META-INF/maven/org.springframework.boot/SpringBootMavenExample; ls'
-            sshGet remote: item, from:''+"${appname}"+'/temp/META-INF/maven/org.springframework.boot/SpringBootMavenExample/pom.properties', into: 'info/'+"${item.name}"+"-"+"${item.host}"+'.txt', override: true
+            sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/'+"${appname}"+'/temp/META-INF/maven/org.springframework.boot/'+"${artifact}"+'; ls'
+            sshGet remote: item, from:''+"${appname}"+'/temp/META-INF/maven/org.springframework.boot/'+"${artifact}"+'/pom.properties', into: 'info/'+"${item.name}"+"-"+"${item.host}"+'.txt', override: true
             sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/'+"${appname}"+'; rm -R temp', failOnError:'false'
           }
       }
