9c8f31cc1d0be58d0db67b10fb7047411af39d90
yashbhangdia <55742719+yashbhangdia@users.noreply.github.com>
2021-07-15 10:21:05 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -4,18 +4,21 @@ def call(url)
 {
   def remote = getVM("${url}")
   
+  def applist = data.app()
+  def appname = applist["${url}"]
+  
   remote.each{
         item -> L:{
             echo "Deploying to : ${item.name}"
 
-            withCredentials([sshUserPrivateKey(credentialsId: item.id, keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: item.name)]) {
+          withCredentials([sshUserPrivateKey(credentialsId: item.id, keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: item.name)]) {
           item.user = item.name
           item.identityFile = identity
-          sshCommand remote: item, command: 'rm -R deployment', failOnError:'false'
-          sshCommand remote: item, command: 'mkdir deployment', failOnError:false
-          sshCommand remote : item, command: 'cd /home/'+"${item.name}"+'/deployment; PID=`ps -eaf | grep "java -jar" | grep -v grep | awk \'{print $2}\'`; if [[ "" !=  "$PID" ]]; then echo "found $PID and killing it!!" ; kill -9 $PID; fi'
-          sshPut remote: item, from: "./target", filterRegex: /.jar$/, into: '/home/'+"${item.name}"+'/deployment/'
-          sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/deployment/target; nohup java -jar *.jar >> app.log &'
+          sshCommand remote : item, command: 'cd /home/'+"${item.name}"+'/'+"${appname}"+'/target; TEMP=`find *.jar | awk \'{print $1}\'`; PID=`ps -eaf | grep "java -jar "$TEMP"" | grep -v grep | awk \'{print $2}\'`; if [[ "" !=  "$PID" ]]; then echo "found $PID and killing it!!" ; kill -9 $PID; fi'
+          sshCommand remote: item, command: 'rm -R '+"${appname}", failOnError:'false'
+          sshCommand remote: item, command: 'mkdir '+"${appname}", failOnError:false
+          sshPut remote: item, from: "./target", filterRegex: /.jar$/, into: '/home/'+"${item.name}"+'/'+"${appname}"+'/'
+          sshCommand remote: item, command: 'cd /home/'+"${item.name}"+'/'+"${appname}"+'/target; nohup java -jar *.jar >> app.log &'
             }
         }
     }
