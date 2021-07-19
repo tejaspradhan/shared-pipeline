b76ae82566ff322474768126258fd342a3abf8e4
Shriram Pareek <66828879+Shrirampareek888@users.noreply.github.com>
2021-07-19 16:01:21 +0530

    Update liquibaseUpdate.groovy

--- vars/liquibaseUpdate.groovy
+++ vars/liquibaseUpdate.groovy
@@ -5,7 +5,6 @@ def call()
   bat label: '', script: 'mvn liquibase:update'
           
   withCredentials([ usernamePassword(credentialsId: 'MySQL', usernameVariable: 'username', passwordVariable: 'password')]) {
-    //print 'username=' + username + 'password=' + password
     bat label: '', script: 'mysql -u'+username+' -p'+password+' -h192.168.43.146 -e  "SELECT AUTHOR, ID, DATEEXECUTED, EXECTYPE, DEPLOYMENT_ID FROM liquibase.databasechangelog ORDER BY ORDEREXECUTED DESC LIMIT 1" > dbcheck.txt '
   }
 }
