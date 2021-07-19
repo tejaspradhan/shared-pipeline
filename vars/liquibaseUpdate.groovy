e8ea8b5135c061d267e9c8a78a8094de36447594
yashbhangdia <55742719+yashbhangdia@users.noreply.github.com>
2021-07-19 16:00:56 +0530

    Create liquibaseUpdate.groovy

---
+++ vars/liquibaseUpdate.groovy
@@ -0,0 +1,11 @@
+#!usr/bin/env groovy
+
+def call()
+{
+  bat label: '', script: 'mvn liquibase:update'
+          
+  withCredentials([ usernamePassword(credentialsId: 'MySQL', usernameVariable: 'username', passwordVariable: 'password')]) {
+    //print 'username=' + username + 'password=' + password
+    bat label: '', script: 'mysql -u'+username+' -p'+password+' -h192.168.43.146 -e  "SELECT AUTHOR, ID, DATEEXECUTED, EXECTYPE, DEPLOYMENT_ID FROM liquibase.databasechangelog ORDER BY ORDEREXECUTED DESC LIMIT 1" > dbcheck.txt '
+  }
+}
